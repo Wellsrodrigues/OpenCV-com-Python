e3889cc5f2588ce3d3a01f36ceb88e4a429790f2
Wellison Rodrigues <wellisonrodrigues2001@gmail.com>
2023-05-10 08:37:59 -0300

    up

--- 31-Morphology.py
+++ 31-Morphology.py
@@ -6,8 +6,11 @@ img = cv2.imread('atividade_aula11.png', cv2.IMREAD_GRAYSCALE)
 img = cv2.resize(img,(200,200), interpolation = cv2.INTER_CUBIC)
 
 # Img1: erosao
-kernel_e = cv2.getStructuringElement(cv2.MORPH_RECT,(7,7))
-erosion_e = cv2.erode(img, kernel_e, iterations=4)
+# kernel_e = cv2.getStructuringElement(cv2.MORPH_RECT,(7,7))
+kernel_e = np.zeros((3,3), dtype=np.uint8) 
+# kernel personalizado
+kernel_e[0][0] = 1
+erosion_e = cv2.erode(img, kernel_e, iterations=7)
 
 
 # Img2: dilatacao
@@ -20,7 +23,8 @@ dilated_d = cv2.dilate(erosion_d, dilation_d)
 # Img 3: hiper dilatacao
 kernel_h = cv2.getStructuringElement(cv2.MORPH_RECT,(9,9))
 dilated_h = cv2.dilate(img, (kernel_h), iterations=5)
-kernel_hp = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
+kernel_hp = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9
+                                                         ,9))
 eroded_h= cv2.erode(dilated_h, kernel_hp, iterations=5)
 dilation_h = cv2.dilate(eroded_h, kernel_hp, iterations=3)
 
